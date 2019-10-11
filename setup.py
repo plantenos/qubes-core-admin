92826d255909af9d132f74fdf52e9b3116164e0f
Plan 10 <plantenos@protonmail.com>
2019-10-11 08:47:18 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ import setuptools.command.install
 # don't import: import * is unreliable and there is no need, since this is
 # compile time and we have source files
 def get_console_scripts():
-    for filename in os.listdir('./qubes/tools'):
+    for filename in os.listdir('./plan10/tools'):
         basename, ext = os.path.splitext(os.path.basename(filename))
         if basename == '__init__' or ext != '.py':
             continue
-        yield basename.replace('_', '-'), 'qubes.tools.{}'.format(basename)
+        yield basename.replace('_', '-'), 'plan10.tools.{}'.format(basename)
 
 # create simple scripts that run much faster than "console entry points"
 class CustomInstall(setuptools.command.install.install):
@@ -40,52 +40,52 @@ if __name__ == '__main__':
 
 if __name__ == '__main__':
     setuptools.setup(
-        name='qubes',
+        name='plan10',
         version=open('version').read().strip(),
         author='Invisible Things Lab',
         author_email='woju@invisiblethingslab.com',
         description='Qubes core package',
         license='GPL2+',
-        url='https://www.qubes-os.org/',
+        url='https://www.plan10-os.org/',
         packages=setuptools.find_packages(exclude=('core*', 'tests')),
         cmdclass={
             'install': CustomInstall,
         },
         entry_points={
-            'qubes.vm': [
-                'AppVM = qubes.vm.appvm:AppVM',
-                'TemplateVM = qubes.vm.templatevm:TemplateVM',
-                'StandaloneVM = qubes.vm.standalonevm:StandaloneVM',
-                'AdminVM = qubes.vm.adminvm:AdminVM',
-                'DispVM = qubes.vm.dispvm:DispVM',
+            'plan10.vm': [
+                'AppVM = plan10.vm.appvm:AppVM',
+                'TemplateVM = plan10.vm.templatevm:TemplateVM',
+                'StandaloneVM = plan10.vm.standalonevm:StandaloneVM',
+                'AdminVM = plan10.vm.adminvm:AdminVM',
+                'DispVM = plan10.vm.dispvm:DispVM',
             ],
-            'qubes.ext': [
-                'qubes.ext.admin = qubes.ext.admin:AdminExtension',
-                'qubes.ext.core_features = qubes.ext.core_features:CoreFeatures',
-                'qubes.ext.qubesmanager = qubes.ext.qubesmanager:QubesManager',
-                'qubes.ext.gui = qubes.ext.gui:GUI',
-                'qubes.ext.r3compatibility = qubes.ext.r3compatibility:R3Compatibility',
-                'qubes.ext.pci = qubes.ext.pci:PCIDeviceExtension',
-                'qubes.ext.block = qubes.ext.block:BlockDeviceExtension',
-                'qubes.ext.services = qubes.ext.services:ServicesExtension',
-                'qubes.ext.windows = qubes.ext.windows:WindowsFeatures',
+            'plan10.ext': [
+                'plan10.ext.admin = plan10.ext.admin:AdminExtension',
+                'plan10.ext.core_features = plan10.ext.core_features:CoreFeatures',
+                'plan10.ext.plan10manager = plan10.ext.plan10manager:QubesManager',
+                'plan10.ext.gui = plan10.ext.gui:GUI',
+                'plan10.ext.r3compatibility = plan10.ext.r3compatibility:R3Compatibility',
+                'plan10.ext.pci = plan10.ext.pci:PCIDeviceExtension',
+                'plan10.ext.block = plan10.ext.block:BlockDeviceExtension',
+                'plan10.ext.services = plan10.ext.services:ServicesExtension',
+                'plan10.ext.windows = plan10.ext.windows:WindowsFeatures',
             ],
-            'qubes.devices': [
-                'pci = qubes.ext.pci:PCIDevice',
-                'block = qubes.ext.block:BlockDevice',
-                'testclass = qubes.tests.devices:TestDevice',
+            'plan10.devices': [
+                'pci = plan10.ext.pci:PCIDevice',
+                'block = plan10.ext.block:BlockDevice',
+                'testclass = plan10.tests.devices:TestDevice',
             ],
-            'qubes.storage': [
-                'file = qubes.storage.file:FilePool',
-                'file-reflink = qubes.storage.reflink:ReflinkPool',
-                'linux-kernel = qubes.storage.kernels:LinuxKernel',
-                'lvm_thin = qubes.storage.lvm:ThinPool',
+            'plan10.storage': [
+                'file = plan10.storage.file:FilePool',
+                'file-reflink = plan10.storage.reflink:ReflinkPool',
+                'linux-kernel = plan10.storage.kernels:LinuxKernel',
+                'lvm_thin = plan10.storage.lvm:ThinPool',
             ],
-            'qubes.tests.storage': [
-                'test = qubes.tests.storage:TestPool',
-                'file = qubes.storage.file:FilePool',
-                'file-reflink = qubes.storage.reflink:ReflinkPool',
-                'linux-kernel = qubes.storage.kernels:LinuxKernel',
-                'lvm_thin = qubes.storage.lvm:ThinPool',
+            'plan10.tests.storage': [
+                'test = plan10.tests.storage:TestPool',
+                'file = plan10.storage.file:FilePool',
+                'file-reflink = plan10.storage.reflink:ReflinkPool',
+                'linux-kernel = plan10.storage.kernels:LinuxKernel',
+                'lvm_thin = plan10.storage.lvm:ThinPool',
             ],
         })
