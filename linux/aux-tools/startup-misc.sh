13134eec8e1b8d37b21a293027131bf173e65025
Plan 10 <plantenos@protonmail.com>
2019-10-11 09:03:21 -0700

    Update startup-misc.sh

--- linux/aux-tools/startup-misc.sh
+++ linux/aux-tools/startup-misc.sh
@@ -2,14 +2,14 @@
 
 # Misc dom0 startup setup
 
-/usr/lib/qubes/fix-dir-perms.sh
+/usr/lib/plan10/fix-dir-perms.sh
 DOM0_MAXMEM=$(/usr/sbin/xl list 0 | tail -1 | awk '{ print $3 }')
 xenstore-write /local/domain/0/memory/static-max $[ $DOM0_MAXMEM * 1024 ]
 
 xl sched-credit -d 0 -w 2000
-cp /var/lib/qubes/qubes.xml /var/lib/qubes/backup/qubes-$(date +%F-%T).xml
+cp /var/lib/plan10/plan10.xml /var/lib/plan10/backup/plan10-$(date +%F-%T).xml
 
-/usr/lib/qubes/cleanup-dispvms
+/usr/lib/plan10/cleanup-dispvms
 
 # Hide mounted devices from qubes-block list (at first udev run, only / is mounted)
 udevadm trigger --action=change --subsystem-match=block
