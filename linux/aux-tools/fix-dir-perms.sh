1572dc89fb04c58d8bd8b60fdfd8c14beca72191
Plan 10 <plantenos@protonmail.com>
2019-10-11 08:54:18 -0700

    Update fix-dir-perms.sh

--- linux/aux-tools/fix-dir-perms.sh
+++ linux/aux-tools/fix-dir-perms.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-chgrp qubes /etc/xen
+chgrp plan10 /etc/xen
 chmod 710 /etc/xen
-chgrp qubes /var/run/xenstored/*
+chgrp plan10 /var/run/xenstored/*
 chmod 660 /var/run/xenstored/*
-chgrp qubes /var/lib/xen
+chgrp plan10 /var/lib/xen
 chmod 770 /var/lib/xen
-chgrp qubes /var/log/xen
+chgrp plan10 /var/log/xen
 chmod 770 /var/log/xen
-chgrp qubes /proc/xen/privcmd
+chgrp plan10 /proc/xen/privcmd
 chmod 660 /proc/xen/privcmd
-chgrp qubes /proc/xen/xenbus
+chgrp plan10 /proc/xen/xenbus
 chmod 660 /proc/xen/xenbus
-chgrp qubes /dev/xen/evtchn
+chgrp plan10 /dev/xen/evtchn
 chmod 660 /dev/xen/evtchn
-chgrp -R qubes /var/log/xen
+chgrp -R plan10 /var/log/xen
 chmod -R g+rX /var/log/xen
 chmod g+s /var/log/xen/console
